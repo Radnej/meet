47a98f0217bb5679c9529e38e7a3828fea53f645
Radnej <91905344+Radnej@users.noreply.github.com>
2022-07-04 13:00:13 +0200

    code reverted to the default state/Eventtest & NumberOfEvents deleted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
-import NumberOfEvents from "./NumberOfEvents";
 
 class App extends Component {
   render() {
@@ -10,7 +9,6 @@ class App extends Component {
       <div className="App">
         <CitySearch />
         <EventList />
-        <NumberOfEvents />
       </div>
     );
   }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -18,8 +18,4 @@ describe("<App/> component", () => {
   test("render CitySearch", () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
-
-  test("render NumberOfEvents", () => {
-    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
-  });
 });
