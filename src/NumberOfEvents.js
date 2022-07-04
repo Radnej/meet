370109853240f0b15a935c87ad14fbeeef2ad887
Radnej <91905344+Radnej@users.noreply.github.com>
2022-07-04 16:01:16 +0200

    create NumberOfEvents.test.js to test component of NumberOfEvent.js,add input type/added NumberOfEvents.js,add test render text input/add NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
   render() {
-    return <div className="numberOfEvents"></div>;
+    return (
+      <div className="numberOfEvents">
+        <input className="number events_number_input" type="number"></input>
+      </div>
+    );
   }
 }
 
