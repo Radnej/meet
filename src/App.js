800eca84fd987da2f87a20c7c089318325c83e9d
Radnej <91905344+Radnej@users.noreply.github.com>
2022-07-06 22:13:13 +0200

    Getting the Access Token/api.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import "./nprogress.css";
 import React, { Component } from "react";
 import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations } from "./api";
+import { getEvents } from "./api";
 
 class App extends Component {
   state = {
