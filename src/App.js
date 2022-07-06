038e7603ed10b41d803c941edebdbef35d02628d
Radnej <91905344+Radnej@users.noreply.github.com>
2022-07-06 19:46:54 +0200

    import mockData/imported api.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
+import { mockData } from "./mock-data";
 
 class App extends Component {
   state = {
