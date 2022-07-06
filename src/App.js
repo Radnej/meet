16e40d7c3d870fba5796fec99ed403a05237bfe0
Radnej <91905344+Radnej@users.noreply.github.com>
2022-07-06 20:16:48 +0200

    create new method to change the events state/created App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
-import { mockData } from "./mock-data";
+import { getEvents } from "./api";
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
   };
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter(
+        (event) => event.location === location
+      );
+      this.setState({
+        events: locationEvents,
+      });
+    });
+  };
   render() {
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />{" "}
         <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
