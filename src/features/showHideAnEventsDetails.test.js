e541fae48bdbf40f3b6b65e4f06882d7c3ca1d11
Radnej <91905344+Radnej@users.noreply.github.com>
2022-07-17 17:52:28 +0200

    create file to Specify number of events/created Specify number of events. feature

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -7,35 +7,60 @@ import { mockData } from "../mock-data";
 const feature = loadFeature("./src/features/showHideAnEventsDetails.feature");
 
 defineFeature(feature, (test) => {
-    let AppWrapper;
+  let AppWrapper;
 
-    test('An event element is collapsed by default.', ({ given, when, then }) => {
-        given('The user is viewing the list of events', () => {
-            AppWrapper = mount(<App />);
-        });
-
-        when('The user hasn’t clicked on event element', () => {
-            AppWrapper.update();
-            expect(AppWrapper.find('.event')).toHaveLength(mockEvents.length);
-        });
-
-        then('The event details are collapsed', () => {
-            expect(AppWrapper.find('.event.description')).toHaveLength(0)
-        });
+  test("An event element is collapsed by default.", ({ given, when, then }) => {
+    given("The user is viewing the list of events", () => {
+      AppWrapper = mount(<App />);
     });
 
-    test('User can expand an event to see its details', ({ given, when, then }) => {
-        given('The user is viewing the list of events', async () => {
-            AppWrapper = await mount(<App />);
-        });
-
-        when('The user has clicked on event element', () => {
-            AppWrapper.update();
-            AppWrapper.find('.event .button').at(0).simulate('click');
-        });
-
-        then('The event details are expanded', () => {
-            expect(AppWrapper.find('.event .event.description')).toHaveLength(1);
-        });
+    when("The user hasn’t clicked on event element", () => {
+      AppWrapper.update();
+      expect(AppWrapper.find(".event")).toHaveLength(mockData.length);
     });
 
+    then("The event details are collapsed", () => {
+      expect(AppWrapper.find(".event.description")).toHaveLength(0);
+    });
+  });
+
+  test("User can expand an event to see its details", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("The user is viewing the list of events", async () => {
+      AppWrapper = await mount(<App />);
+    });
+
+    when("The user has clicked on event element", () => {
+      AppWrapper.update();
+      AppWrapper.find(".event .button").at(0).simulate("click");
+    });
+
+    then("The event details are expanded", () => {
+      expect(AppWrapper.find(".event .event.description")).toHaveLength(1);
+    });
+  });
+
+  test("User can collapse an event to hide its details", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("The user no longer wants to see the events details", async () => {
+      AppWrapper = await mount(<App />);
+      AppWrapper.update();
+      AppWrapper.find(".event .button").at(0).simulate("click");
+      expect(AppWrapper.find(".event .event.description")).toHaveLength(1);
+    });
+
+    when("The user clicks on the details", () => {
+      AppWrapper.find(".event .button").at(0).simulate("click");
+    });
+
+    then("The event details can be collapsed", () => {
+      expect(AppWrapper.find(".event .event.description")).toHaveLength(0);
+    });
+  });
+});
---
+++ src/features/specifyNumberOfEvents.feature
@@ -0,0 +1,11 @@
+Feature: SPECIFY NUMBER OF EVENTS
+
+Scenario : When user hasn’t specified a number, 32 is the default number
+Given: The user hasn’t specified the number of events
+When: The user begins their search
+Then: 32 events are listed
+
+Scenario: User can change the number of events they want to see
+Given: The user has specified the number of events
+When: The user has chosen how many events to see
+Then: The number of displayed events matches the number chosen by user
