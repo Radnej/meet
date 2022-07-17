62de4167570efaa8af5a654399ea3a57e6ce4032
Radnej <91905344+Radnej@users.noreply.github.com>
2022-07-17 17:17:24 +0200

    write test for An event element is collapsed by default/showHideAnEventsDetails.test.js

---
+++ src/features/showHideAnEventsDetails.test.js
@@ -0,0 +1,25 @@
+import { loadFeature, defineFeature } from "jest-cucumber";
+import React from "react";
+import { mount } from "enzyme";
+import App from "../App";
+import { mockData } from "../mock-data";
+
+const feature = loadFeature("./src/features/showHideAnEventsDetails.feature");
+
+defineFeature(feature, (test) => {
+    let AppWrapper;
+
+    test('An event element is collapsed by default.', ({ given, when, then }) => {
+        given('The user is viewing the list of events', () => {
+            AppWrapper = mount(<App />);
+        });
+
+        when('The user hasn’t clicked on event element', () => {
+            AppWrapper.update();
+            expect(AppWrapper.find(".event")).toHaveLength(mockEvents.length);
+        });
+
+        then('The event details are collapsed', () => {
+            expect(AppWrapper.find('.event.description')).toHaveLength(0)
+        });
+    });
