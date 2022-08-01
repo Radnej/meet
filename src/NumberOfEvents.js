be5b1bf2402d6e8c06a93eab54cf1b164b84053d
Radnej <91905344+Radnej@users.noreply.github.com>
2022-08-01 19:32:25 +0200

    import Alert/NumberOfEvents.js,add code inside handleInputChanged function to render Alert/ NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 15,
   };
 
+  //   handleInputChanged = (event) => {
+  //     const value = event.target.value;
+  //     this.props.updateEvents(null, value);
+  //     this.setState({ numberOfEvents: value });
+  //   };
+
   handleInputChanged = (event) => {
-    const value = event.target.value;
-    this.props.updateEvents(null, value);
-    this.setState({ numberOfEvents: value });
+    let inputValue = event.target.value;
+    if (inputValue >= 15 || inputValue <= 0) {
+      this.setState({
+        numberOfEvents: inputValue,
+        infoText: "Please enter a number between 1 - 15",
+      });
+    } else {
+      this.setState({
+        numberOfEvents: event.target.value,
+        infoText: " ",
+      });
+    }
+
+    this.props.updateEvents(undefined, inputValue);
   };
 
   render() {
@@ -21,6 +39,7 @@ class NumberOfEvents extends Component {
           value={numberOfEvents}
           onChange={this.handleInputChanged}
         ></input>
+        <ErrorAlert text={this.state.infoText} />
       </div>
     );
   }
