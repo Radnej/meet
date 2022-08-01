109884eebf26d04c07a6f8e4f89e51eda17cbfc1
Radnej <91905344+Radnej@users.noreply.github.com>
2022-08-01 19:15:04 +0200

    create Info Alert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { InfoAlert } from "./Alert";
 
 class CitySearch extends Component {
   state = {
@@ -6,16 +7,38 @@ class CitySearch extends Component {
     suggestions: [],
     showSuggestions: undefined,
   };
+  //   handleInputChanged = (event) => {
+  //     const value = event.target.value;
+  //     const suggestions = this.props.locations.filter((location) => {
+  //       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+  //     });
+  //     this.setState({
+  //       query: value,
+  //       suggestions,
+  //     });
+  //   };
+
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText:
+          "We can not find the city you are looking for. Please try another city",
+      });
+    } else {
+      this.setState({
+        query: value,
+        suggestions,
+        infoText: "",
+      });
+    }
   };
+
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
@@ -27,6 +50,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <p className="SearchCity">Choose nearest city</p>
         <input
           type="text"
