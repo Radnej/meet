0d73f537dc467c294c7674964594fee56a7df9a3
Radnej <91905344+Radnej@users.noreply.github.com>
2022-08-07 09:29:48 +0200

    update App.js,add privacy

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import React, { Component } from "react";
 import NumberOfEvents from "./NumberOfEvents";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
-
-import { getEvents, extractLocations } from "./api";
+import WelcomeScreen from "./WelcomeScreen";
+import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
 
 class App extends Component {
   state = {
@@ -13,18 +13,35 @@ class App extends Component {
     locations: [],
     currentLocation: "all",
     numberOfEvents: 15,
+    showWelcomeScreen: undefined,
   };
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   this.mounted = true;
+  //   getEvents().then((events) => {
+  //     if (this.mounted) {
+  //       this.setState({
+  //         events: events.slice(0, 15),
+  //         locations: extractLocations(events),
+  //       });
+  //     }
+  //   });
+  // }
+
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({
-          events: events.slice(0, 15),
-          locations: extractLocations(events),
-        });
-      }
-    });
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
   }
 
   componentWillUnmount() {
@@ -50,6 +67,9 @@ class App extends Component {
   };
 
   render() {
+    const { showWelcomeScreen } = this.state;
+
+    if (!showWelcomeScreen) return <div className="App" />;
     return (
       <div className="App">
         <CitySearch
@@ -58,6 +78,12 @@ class App extends Component {
         />{" "}
         <NumberOfEvents updateEvents={this.updateEvents} />
         <EventList events={this.state.events} />
+        <WelcomeScreen
+          showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken();
+          }}
+        />
       </div>
     );
   }
