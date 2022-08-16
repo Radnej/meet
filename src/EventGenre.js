06861b896b4699d72ab69fafeedb52b7a60f7ed9
Radnej <91905344+Radnej@users.noreply.github.com>
2022-08-16 22:34:54 +0200

    fix style

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -5,21 +5,21 @@ const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    const getData = () => {
+      const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
+      const data = genres.map((genre) => {
+        const value = events.filter((event) =>
+          event.summary.split(" ").includes(genre)
+        ).length;
+        return { name: genre, value };
+      });
+      return data;
+    };
+
     setData(() => getData());
   }, [events]);
 
   const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#6A0DAD"];
-
-  const getData = () => {
-    const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
-    const data = genres.map((genre) => {
-      const value = events.filter((event) =>
-        event.summary.split(" ").includes(genre)
-      ).length;
-      return { name: genre, value };
-    });
-    return data;
-  };
 
   return (
     <ResponsiveContainer height={400}>
